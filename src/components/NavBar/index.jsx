2ab32138eac7d3b6caa7234db94cbd566a045f54
Felipe Lourenco <lipelourencosilva@gmail.com>
2024-06-08 21:32:17 -0300

    add: navbar implementation

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,32 @@
+import { Link, useLocation } from "react-router-dom";
+import "./styles.css"
+
+export default function NavBar() {
+  const location = useLocation();
+  return (
+    <div className="navbar-container">
+        <div className="navbar-line1"></div>
+        <nav className="navbar">
+            
+            <ul>
+                <li>
+                    <Link id={location.pathname === "/" ? "selected-path" : ""} to="/">Home</Link>
+                </li>
+                <li>
+                    <Link id={location.pathname === "/cryptos" ? "selected-path" : ""} to="/cryptos">Cryptos</Link>
+                </li>
+                <li>
+                    <Link id={location.pathname === "/market" ? "selected-path" : ""} to="/market">Market</Link>
+                </li>
+                <li>
+                    <Link id={location.pathname === "/prices" ? "selected-path" : ""} to="/prices">Pricing</Link>
+                </li>
+                <li>
+                    <Link id={location.pathname === "/contact" ? "selected-path" : ""} to="/cryptos">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+        <div className="navbar-line2"></div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import './styles/globals.css'
+import HomePage from './pages/Home/index.jsx'
+import NavBar from './components/NavBar/index.jsx'
+import Layout from './components/Layout/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />, 
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
